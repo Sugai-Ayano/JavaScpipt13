3a0c8330c3d9f722d63298fe623ec0625b8938bc
GitHubのユーザ名 <GitHub>
2020-12-10 10:46:34 +0000

    [Start]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '4fc3102dc10e7b643190e054019c00fcb13e13172e3abfb92b5405f7f64214f3b717ca198e5abeb28e2b8adc18cc477d985f9719e8cb0c6622f6f1ed8a3bb954'
